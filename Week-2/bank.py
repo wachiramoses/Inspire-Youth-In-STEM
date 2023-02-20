d080f464d352833eb4b6ae39907489bbbaaaa289
wachiramoses <wachiramoses519@gmail.com>
2023-02-20 11:11:19 +0300

    tax and gross income

--- Week-2/bank.py
+++ Week-2/bank.py
@@ -16,4 +16,27 @@
 #5% if income is less 100k
 
 #print gross income ,net income
-income = input("enter your income")
+gross_income =int(input("what is your gross income"))
+tax_group_1 = (gross_income * 0.05)
+tax_group_2 = (gross_income * 0.16 )
+tax_group_3 = (gross_income * 0.19)
+tax_group_4 = (gross_income * 0.3)
+
+if gross_income < 100000:
+    print("gross income:", gross_income)
+    print("net income ", gross_income - tax_group_1)
+
+elif (gross_income > 100000) & (gross_income < 150000):
+    print("gross_income",gross_income)
+    print("net_income:", gross_income - tax_group_2)
+
+elif(gross_income <=150001) & (gross_income <100000):
+    print("gross_income = ",gross_income)
+    print("tax = ",(gross_income - tax_group_3))
+    print("net_income:", gross_income - tax_group_3)
+
+else:
+    ("gross_income",gross_income)
+    print("net_income:", gross_income - tax_group_4)
+
+
