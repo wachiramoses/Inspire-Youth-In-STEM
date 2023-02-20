1fe42e6862cfef8348272da435f75eff06927a7b
wachiramoses <wachiramoses519@gmail.com>
2023-02-20 09:50:56 +0300

    new week

---
+++ Week-3/assignment-1.py
@@ -0,0 +1,6 @@
+#create an empty list of vavourite musicians
+#favourite_musicians=[]
+#using for loop add 5 new musicians
+#copy the list to a new list calledcelebs
+#sort the listpop out 2 celebrity to the list
+#count the remaining celebs
---
+++ Week-3/lists_revisited.py
@@ -0,0 +1,11 @@
+Friends = ["wachira","mose","jayden","kelvin","paul"]
+    print(friends)
+    friends[4] = "mary"
+    print(friends)
+print("--------------------------------------------")
+new_friends = friends.copy()
+print(new_friends)
+new_friends = friends.sort()
+print(new_friends)
+new_friens.pop()
+print(new_friends)
