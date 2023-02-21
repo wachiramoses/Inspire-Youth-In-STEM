ccc2a1850a67bc4c6946a5f8fbbd74a71dd3ba38
wachiramoses <wachiramoses519@gmail.com>
2023-02-21 08:35:41 +0300

    lists_revisited

--- Week-3/assignment-1.py
+++ Week-3/assignment-1.py
@@ -1,6 +1,43 @@
 #create an empty list of vavourite musicians
-#favourite_musicians=[]
 #using for loop add 5 new musicians
 #copy the list to a new list calledcelebs
 #sort the listpop out 2 celebrity to the list
 #count the remaining celebs
+
+
+#!usr/bin/env python3
+#This is a single line comment
+#python assignment
+#Name : Moses Wachira
+#Email : wachiramoses519@gmail.com
+#Date : 20th Feb 2023
+#File : assignment.py
+
+
+
+#create an empty list of favourite musicians
+fav_musicians = []
+
+#using for loop add five new musicians
+print("Enter five names of your favourite musicians:")
+for musician in range(0,5):
+    the_musicians = input("Enter name:")
+    fav_musicians.append(the_musicians)
+print(fav_musicians)
+
+#copy the list to a new list called celebs 
+celebs = fav_musicians.copy()
+print(celebs)
+
+#sort the list
+celebs.sort()
+print(celebs)
+
+#pop out two celebrities
+celebs.pop()
+celebs.pop()
+print(celebs)
+
+#count the remaining celebrities in the list
+print(len(celebs))
+
--- Week-3/lists_revisited.py
+++ Week-3/lists_revisited.py
@@ -1,11 +1,29 @@
-Friends = ["wachira","mose","jayden","kelvin","paul"]
-    print(friends)
-    friends[4] = "mary"
-    print(friends)
-print("--------------------------------------------")
+#!usr/bin/env python3
+#This is a single line comment
+#python program on lists revisited
+#Name : Moses Wachira
+#Email : wachiramoses519@gmail.com
+#Date : 21st Feb 2023
+#File : lists_revisited.py
+
+
+myFavouriteFruits = ["banana","apple","mango","lime","avocado"]
+
+for fruit in myFavouriteFruits:
+    print(fruit)
+
+print(len(myFavouriteFruits))
+
+friends = ["wachira","mose","jayden","kelvin","paul"]
+print(friends)
+friends[5] = "mary"
+print(friends)
+print("-----------------------------------------------------")
 new_friends = friends.copy()
 print(new_friends)
-new_friends = friends.sort()
+
+new_friends.sort()
 print(new_friends)
-new_friens.pop()
+
+new_friends.pop()
 print(new_friends)
