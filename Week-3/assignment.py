42b8213bab637117360f502b958b270e6034dd9c
wachiramoses <wachiramoses519@gmail.com>
2023-02-23 12:51:39 +0300

    assignment

--- Week-3/assignment.py
+++ Week-3/assignment.py
@@ -1,2 +1,35 @@
-# write a program to print a number using functions
-#write a program to calculate simple interest
+# write a program factorial of a number using functions
+num = int(input("Enter a number: "))
+
+factorial = 1
+
+if num < 0:
+    print("Factorial does not exist for negative numbers")
+elif num == 0:
+
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1,num + 1):
+        factorial = factorial*i
+    print("The factorial of",num,"is",factorial)
+
+#write a program to calculate simple interestdef simple_interest(p,t,r):
+
+def simple_interest(p,t,r):
+    print('The principal is', p)
+    print('The time period is', t)
+    print('The rate of interest is',r)
+     
+    si = (p * t * r)/100
+     
+    print('The Simple Interest is', si)
+     
+     
+# Driver code
+P = int(input("Enter the principle amount :"))
+T = int(input("Enter the time period :"))
+R = int(input("Enter the rate of interest :"))
+simple_interest(P,T,R)
+
+
+
